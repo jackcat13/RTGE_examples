e6e0288910f1ca30c36b367510bf42d838c615fc
christophe.henry2 <christophe.henry2@soprabanking.com>
2023-08-09 16:03:40 +0200

    feat: examples to print sprites and to print sprites with camera centered on a given sprite

---
+++ README.md
@@ -0,0 +1,8 @@
+# RTGE examples
+
+The purpose of this project is to provide examples of the [RTGE library](https://github.com/jackcat13/RTGE) usage. It aims at showing code samples of every feature.
+
+## Examples list
+
+- `cargo run --bin print_sprite` -> Show a basic usage on how to print a list of sprites
+- `cargo run --bin print_centered_on_sprite` -> Show how to center the camera on a sprite and to print a list of sprites
---
+++ src/bin/print_sprite/main.rs
@@ -0,0 +1,27 @@
+use rtge::{
+    controls::direction::Direction,
+    rendering::{
+        entity::{print_sprites, Entity},
+        position::Position,
+        sprite::load_sprite,
+    },
+};
+
+fn main() {
+    let bob = Entity {
+        name: "bob".to_string(),
+        sprite: load_sprite("./src/bin/print_sprite/sprites/bob.json".to_string()),
+        position: Position { x: 10, y: 10 },
+        direction: Direction {
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        },
+        speed: 2,
+    };
+    let mut another_bob = bob.clone();
+    another_bob.position.x = 100;
+    let entities = vec![bob, another_bob];
+    print_sprites(&entities);
+}
